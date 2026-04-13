416f83bc65c752f5c5e85bb224ade5d171f174c4
kentcode-002 <kent.code.delacruz@gmail.com>
2026-04-13 08:26:59 +0800

    fixed (6)

--- app/(ui)/page.tsx
+++ app/(ui)/page.tsx
@@ -2,20 +2,20 @@ import Hero from "@/components/Hero";
 import CategorySection from "@/components/CategorySection";
 import Highlights from "@/components/Highlights";
 
-export const dynamic = "force-dynamic"; // 👈 skip static generation
-
 export default async function Home() {
   let products: any[] = [];
 
   try {
-    const res = await fetch("https://fakestoreapi.com/products", {});
+    const res = await fetch("https://fakestoreapi.com/products", {
+      next: { revalidate: 60 }
+    });
 
     if (!res.ok) {
       throw new Error("Failed to fetch products");
     }
 
-    products = await res.json();
-    console.log("Fetched products:", products);
+    const data = await res.json();
+    products = data;
   } catch (error) {
     console.error("Error fetching products:", error);
     products = [];
